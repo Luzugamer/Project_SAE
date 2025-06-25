a29ee782649c5f0cddd7200b80892d2a83310f3a
Luzugamer <lp962708129@gmail.com>
2025-06-25 10:38:57 -0500

    Update settings.py

--- Project_SAE/settings.py
+++ Project_SAE/settings.py
@@ -6,7 +6,7 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 SECRET_KEY = config('SECRET_KEY')
 
