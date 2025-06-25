36f171430e63a27835e4747ec947e6ea0f7c2370
Luzugamer <lp962708129@gmail.com>
2025-06-25 10:52:09 -0500

    Update urls.py

--- Paginas_Inicio/urls.py
+++ Paginas_Inicio/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', pag_descripcion, name='descripcion'),
+    path('principal/', pag_principal, name='principal'),
 
     path('estudiante/', pag_estu, name='pag_estu'),
     path('profesor/', pag_profe, name='pag_profe'),
